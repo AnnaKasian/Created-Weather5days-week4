1a1cbcc9f404d0cdd0d280649226dd0e789dc3e2
Anna <kasyanania@i.ua>
2022-10-31 00:27:58 +0300

    got

--- src/Weather.js
+++ src/Weather.js
@@ -1,93 +1,71 @@
 import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
-  function handleResponse(response) {
-    console.log(response.data);
-    setTemperature(response.data.main.temp);
-    setReady(true);
-  }
-  if (ready) {
-    let weatherData = {
-      city: "New York",
-      temperature: 19,
-      date: "Sunday 22:00",
-      description: "Cloudy",
-      imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-      humidity: 80,
-      wind: 10,
-    };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "c67e1dea0ba4cf764464b48e6670cb92";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="Weather">
-        <div className="overview">
-          <header className="forecast">Weather Forecast for 5 days</header>
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>Last updated: {weatherData.date}</li>
-            <li>{weatherData.description}</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix weather-temperature">
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <strong>{weatherData.temperature}</strong>
-                <span className="units">
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
-        <br />
-        <form className="mb-3">
+        <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-7">
+            <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control"
-                autoComplete="off"
+                autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
               <input
                 type="submit"
-                value="Current"
-                className="btn btn-success"
+                value="Search"
+                className="btn btn-primary w-100"
               />
-            </div>
-            <div>
-              <a href="https://github.com/AnnaKasian/Created-Weather5days-week4">
-                Open-sourse code
-              </a>
-              by Anna Kasian
             </div>
           </div>
         </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
-    const apiKey = "c67e1dea0ba4cf764464b48e6670cb92";
-    let city = "New York";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
